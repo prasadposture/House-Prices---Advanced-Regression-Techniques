24bdac7144a5baff274838657547f62638ddab03
Prasad Rajesh Posture <95558446+prasadposture@users.noreply.github.com>
2023-05-20 14:05:08 +0530

    Add files via upload

--- house.py
+++ house.py
@@ -74,9 +74,11 @@ with b:
                                                                   '1stFlrSF', '2ndFlrSF','LowQualFinSF', 'GrLivArea',
                                                                   'GarageYrBlt', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
                                                                   'EnclosedPorch','3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal'))
-        fig4 = plt.figure()
-        sns.kdeplot(df[option6])
-        st.pyplot(fig4)
+        #fig4 = plt.figure()
+        #sns.kdeplot(df[option6])
+        #st.pyplot(fig4)
+        fig4 = px.histogram(df, x=option6 )
+        st.plotly_chart(fig4, use_container_width=True)
 
 st.write("___")
 c,d = st.columns(2)
@@ -97,54 +99,8 @@ with c:
     fig5 = px.sunburst(xdf,path=option7, values='Number of Houses', color='Number of Houses')
     st.plotly_chart(fig5, use_container_width=True)
 
-HPP=jb.load('house_price_predictor.joblib')
-model1 = HPP['model1']
-model2 = HPP['model2']
-imputer = HPP['imputer']
-scaler = HPP['scaler']
-encoder = HPP['encoder']
-input_cols = HPP['input_cols']
-target_col = HPP['target_col']
-numeric_cols = HPP['numeric_cols']
-categorical_cols = HPP['categorical_cols']
-encoded_cols = HPP['encoded_cols']
-log_numeric_cols = HPP['log_numeric_cols']
-
-with d:
-    st.write("Predicions")
-    tab5, tab6 = st.tabs(["Categorical Input","Numerical Input"])
-    with tab5:
-        categorical_input = { 'MSZoning': 'RL', 'Street': 'Pave', 'Alley': None, 'LotShape': 'IR1', 'LandContour': 'Lvl', 'Utilities': 'AllPub',
-                             'LotConfig': 'Inside', 'LandSlope': 'Gtl', 'Neighborhood': 'NAmes', 'Condition1': 'Norm', 'Condition2': 'Norm',
-                             'BldgType': '1Fam', 'HouseStyle': '1Story', 'RoofStyle': 'Gable', 'RoofMatl': 'CompShg', 'Exterior1st': 'Plywood',
-                             'Exterior2nd': 'Plywood', 'MasVnrType': 'None','ExterQual': 'TA','ExterCond': 'TA',
-                             'Foundation': 'CBlock','BsmtQual': 'TA','BsmtCond': 'TA','BsmtExposure': 'No','BsmtFinType1': 'ALQ',
-                             'Heating': 'GasA','HeatingQC': 'Fa','CentralAir': 'Y','Electrical': 'SBrkr', 'KitchenQual': 'TA','Functional': 'Typ',
-                             'FireplaceQu': np.nan,'GarageType': np.nan,'GarageYrBlt': np.nan,'GarageFinish': np.nan,
-                             'GarageQual': np.nan,'GarageCond': np.nan,'PavedDrive': 'Y',  'PoolQC': np.nan, 'Fence': np.nan, 'MiscFeature': 'Shed',
-                             'SaleType': 'WD', 'SaleCondition': 'Normal'}
-        categorical_input_df = pd.DataFrame([categorical_input])
-        st.write(categorical_input_df)
-
-    with tab6:
-        numerical_input={ 'MSSubClass': 20,'LotFrontage': 77.0, 'LotArea': 9320,
-                          'OverallQual': 4, 'OverallCond': 5, 'YearBuilt': 1959,
-                          'YearRemodAdd': 1959,'MasVnrArea': 0.0,'MiscVal': 400, 'MoSold': 1, 'YrSold': 2010,
-                          'BsmtFinSF1': 569,'BsmtFinType2': 'Unf','BsmtFinSF2': 0,'BsmtUnfSF': 381,
-                          'TotalBsmtSF': 950,'1stFlrSF': 1225, '2ndFlrSF': 0, 'LowQualFinSF': 0, 'GrLivArea': 1225,
-                          'BsmtFullBath': 1, 'BsmtHalfBath': 0, 'FullBath': 1, 'HalfBath': 1, 'BedroomAbvGr': 3, 'KitchenAbvGr': 1,
-                          'Fireplaces': 0,'TotRmsAbvGrd': 6,'GarageCars': 0,'GarageArea': 0,'WoodDeckSF': 352, 'OpenPorchSF': 0,
-                          'EnclosedPorch': 0,'3SsnPorch': 0, 'ScreenPorch': 0, 'PoolArea': 0,
-                          }
-        numerical_input_df=pd.DataFrame([numerical_input])
-        st.write(numerical_input_df)
-    
-    new_df = pd.concat([categorical_input_df, numerical_input_df], axis=1)
-    st.image('OIP.jfif')
-
     
     
-    st.write("#### Predicted Sale Price : $ 120774")
 
 with st.container():
     left, middle, right = st.columns(3)
